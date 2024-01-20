c5cc47aa0e51f6747a0b8a7967cd84144483f480
vgalante2 <vgalante0@gmail.com>
2024-01-20 17:14:43 -0500

    updates

--- components/PoW/Work.jsx
+++ components/PoW/Work.jsx
@@ -14,14 +14,15 @@ function Work() {
     
     <div className={styles.workContainer}>
     <h1 className={styles.workHeading}>
-  
     </h1>
     </div>
+
     <div className={styles.workContent}>
-    <div className={`${styles.workCard} ${styles.Card1}`}><h1 className={styles.FirstH1}>Card 1</h1></div>
-    <div className={`${styles.workCard} ${styles.Card2}`}><h1 className={styles.SecondH1}>Card 2</h1></div>
-    <div className={`${styles.workCard} ${styles.Card3}`}><h1 className={styles.ThirdH1}>Card 3</h1></div>
+    <div className={styles.card}>HOVER</div>
+    <div className={styles.card}>HOVER</div>
+    <div className={styles.card}>HOVER</div>
     </div>
+
     </div>
   )
 }
