db49cee55ddaac6805ee2d484756135f998f2c4e
vgalante2 <vgalante0@gmail.com>
2024-02-19 22:30:57 -0500

    updates

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,94 +1,41 @@
 import styles from "./header.module.scss";
-import React from "react";
+import React, { useEffect} from "react";
 import { useRef} from "react";
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import SplitType from 'split-type'
 import gsap from "gsap";
 import { useGSAP } from '@gsap/react';
 
 function Header() {
 
-    const comp = useRef(null)
 
-    useGSAP(() => {
-      const isMobile = window.innerWidth < 768;
-      
-    let ctx = gsap.context(() => {
-      const t1 = gsap.timeline()
-      t1.from(".relative", {
-        opacity: 0,
-        delay: 0.3,
-        y: isMobile ? "15" : "30" // Smaller movement on mobile
-    }),
-      t1.from("#Title-one", {
-        opacity: 0,
-        delay: 0.3,  
-        y: "+=30"
-      }).from("#Box1", {
-        opacity: 0,
-        delay: 0.3,
-        y: "+=30"
-      }).to(["#Title-one", "#Box1"], {
-        opacity: 0,
-        y: "-=30",
-        delay: 0.3,
-      }).to("#second-slider", {
-       yPercent: "-100",
-        duration: 1.3,
-      }).from("#Title-two", {
-        opacity: 0,
-        delay: 0.3,
-        y: "+=30",
-       }).from("#Box2", {
-        opacity: 0,
-        delay: 0.1,
-        y: "+=30"
-      }).to(["#Title-two", "#Box2"], {
-        opacity: 0,
-        y: "-=30",
-        delay: 0.3,
-      }).to("#fourth-slider", {
-        yPercent: "-200",
-         duration: 1.3,
-      }).from("#Title-four", {
-        opacity: 1,
-        delay: 0.3,
-       
-       }).from("#Box3", {
-        opacity: 0,
-        delay: 0.1,
-        
-      }).from("#info-container", {
-        opacity: 1,
-        delay: 0.3,
-        y: "+=30",
-       }).from(["#info-box1", "#info-box2"], {
-        opacity: 0,
-        delay: 0.3,
-        y: "+=30",
-      }).to("#relative", {
-        clearProps: "opacity, y"
-      })
-    }, comp)
-    
-     return () => ctx.revert();
-    }, [])
+
+  useEffect(() => {
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    const text = new SplitType('#target', { types: 'chars' })
+
+     gsap.set("#target", { autoAlpha: 1 });
+     gsap.set(text.chars, { yPercent: 100 });
+
+     const initalAnimation = gsap.to(text.chars, {
+      yPercent: 0,
+      ease: "sine.out",
+      stagger: {from: "center", amount: 0.5, ease: "power1.out" }
+     });
+  })
+
+
+
 
 
     return(
-        <div id="relative" ref={comp} className={styles.relative}>
-        <div id="intro-slider" className={styles.HeaderContainer}>
-            <h1 id="Title-one" className={styles.headingOne}>Pillar</h1>
-            <div  id="Box1" className={styles.box1}>LABS</div>
-        </div>
-        <div id="second-slider" className={styles.HeaderContainerTwo}>
-            <h1 id="Title-two" className={styles.headingTwo}>Leaving Matrix...</h1>
-            <div id="Box2" className={styles.box2}>NOW</div>
-        </div>
-        <div id="fourth-slider" className={styles.HeaderContainerFour}>
-           <div>
-            <h1 id="Title-three" className={styles.headingThree}>Pillar</h1>
-            <div id="Box3" className={styles.box3}>LABS</div>
-            </div>
-            
+        <div  className={styles.container}>
+        <div  className={styles.HeaderContainer}>
+        <div className={styles.wrapper}></div>
+            <h1 id="target" className={`${styles.pillar} ${styles.logo}`}>Pillar <span id="target" className={`${styles.labs} ${styles.logo}`}>Labs</span></h1>
+           
         </div>
         </div>
     )
