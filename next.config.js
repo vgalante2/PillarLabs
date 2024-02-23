562bce8ee547ba006cec9f01c3b249e2c57e262b
vgalante2 <vgalante0@gmail.com>
2024-02-23 14:36:19 -0500

    gsap debug

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default {
+  transpilePackages: ['gsap'],
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
